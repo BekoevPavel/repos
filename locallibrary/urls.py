d1e8b2aee8f776e9d2d6f2b1624953911d47e412
Pavel Bekoev <pablank@bk.ru>
2019-11-27 23:49:22 +0300

    ff

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+  #  path('catalog/', include('catalog.urls')),
     # path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     # path('accounts/', include('django.contrib.auth.urls')),
 ]
